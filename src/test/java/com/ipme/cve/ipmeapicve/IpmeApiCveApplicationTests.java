97289d49577f8bd4baf1faf6c9f48e64a90678d9
Valentin Geny <vgeny@humanbooster.com>
2019-03-20 11:32:58 +0100

    add travis.yml

--- src/test/java/com/ipme/cve/ipmeapicve/IpmeApiCveApplicationTests.java
+++ src/test/java/com/ipme/cve/ipmeapicve/IpmeApiCveApplicationTests.java
@@ -2,30 +2,18 @@ package com.ipme.cve.ipmeapicve;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.ipme.cve.model.Cve;
-import com.ipme.cve.repository.CveRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IpmeApiCveApplicationTests {
-
-	@Autowired
-	private CveRepository cveRepository;
 	
 	@Test
 	public void contextLoads() {
-		Cve cveTest = new Cve();
-		cveTest = findByTitle("CVE-2008-2252");
-		System.out.println(cveTest.getTitle());
+
 	}
-	
-	@Test
-	public Cve findByTitle(String title) {
-		return cveRepository.findByTitle(title);
-	}
+
 
 }
